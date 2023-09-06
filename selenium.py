7d90e47d7a03f086ce23de37be63d64a7b99e30f
Junaid4ever <110425866+Junaid4ever@users.noreply.github.com>
2023-09-06 22:59:35 +0530

    Update selenium.py

--- selenium.py
+++ selenium.py
@@ -1,90 +1,45 @@
-import asyncio
-import threading
-from concurrent.futures import ThreadPoolExecutor
-from faker import Faker
-from playwright.async_api import async_playwright
-import nest_asyncio
+%%shell
+# Ubuntu no longer distributes chromium-browser outside of snap
+#
+# Proposed solution: https://askubuntu.com/questions/1204571/how-to-install-chromium-without-snap
 
-nest_asyncio.apply()
-fake = Faker('en_US')
+# Add debian buster
+cat > /etc/apt/sources.list.d/debian.list <<'EOF'
+deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster.gpg] http://deb.debian.org/debian buster main
+deb [arch=amd64 signed-by=/usr/share/keyrings/debian-buster-updates.gpg] http://deb.debian.org/debian buster-updates main
+deb [arch=amd64 signed-by=/usr/share/keyrings/debian-security-buster.gpg] http://deb.debian.org/debian-security buster/updates main
+EOF
 
-# Flag to indicate whether the script is running
-running = True
+# Add keys
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
 
-async def start(name, user, wait_time, meetingcode, passcode):
-    print(f"{name} started!")
+apt-key export 77E11517 | gpg --dearmour -o /usr/share/keyrings/debian-buster.gpg
+apt-key export 22F3D138 | gpg --dearmour -o /usr/share/keyrings/debian-buster-updates.gpg
+apt-key export E562B32A | gpg --dearmour -o /usr/share/keyrings/debian-security-buster.gpg
 
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=True, args=['--use-fake-device-for-media-stream', '--use-fake-ui-for-media-stream'])
-        context = await browser.new_context(permissions=['microphone'])
-        page = await context.new_page()
-        await page.goto(f'https://zoom.us/wc/join/{meetingcode}', timeout=200000)
+# Prefer debian repo for chromium* packages only
+# Note the double-blank lines between entries
+cat > /etc/apt/preferences.d/chromium.pref << 'EOF'
+Package: *
+Pin: release a=eoan
+Pin-Priority: 500
 
-        try:
-            await page.click('//button[@id="onetrust-accept-btn-handler"]', timeout=5000)
-        except Exception as e:
-            pass
 
-        try:
-            await page.click('//button[@id="wc_agree1"]', timeout=5000)
-        except Exception as e:
-            pass
+Package: *
+Pin: origin "deb.debian.org"
+Pin-Priority: 300
 
-        try:
-            await page.wait_for_selector('input[type="text"]', timeout=200000)
-            await page.fill('input[type="text"]', user)
-            await page.fill('input[type="password"]', passcode)
-            join_button = await page.wait_for_selector('button.preview-join-button', timeout=200000)
-            await join_button.click()
-        except Exception as e:
-            pass
 
-        try:
-            query = '//button[text()="Join Audio by Computer"]'
-            await asyncio.sleep(13)
-            mic_button_locator = await page.wait_for_selector(query, timeout=350000)
-            await asyncio.sleep(10)
-            await mic_button_locator.evaluate_handle('node => node.click()')
-            print(f"{name} mic aayenge.")
-        except Exception as e:
-            print(f"{name} mic nahe aayenge. ", e)
+Package: chromium*
+Pin: origin "deb.debian.org"
+Pin-Priority: 700
+EOF
 
-        print(f"{name} sleep for {wait_time} seconds ...")
-        while running and wait_time > 0:
-            await asyncio.sleep(1)
-            wait_time -= 1
-        print(f"{name} ended!")
+# Install chromium and chromium-driver
+apt-get update
+apt-get install chromium chromium-driver
 
-        await browser.close()
-
-async def main():
-    global running
-    number = int(input("Enter number of Users: "))
-    meetingcode = input("Enter meeting code (No Space): ")
-    passcode = input("Enter Password (No Space): ")
-
-    sec = 90
-    wait_time = sec * 60
-
-    with ThreadPoolExecutor(max_workers=number) as executor:
-        loop = asyncio.get_running_loop()
-        tasks = []
-        for i in range(number):
-            try:
-                user = fake.name()
-            except IndexError:
-                break
-            task = loop.create_task(start(f'[Thread{i}]', user, wait_time, meetingcode, passcode))
-            tasks.append(task)
-        try:
-            await asyncio.gather(*tasks)
-        except KeyboardInterrupt:
-            running = False
-            # Wait for tasks to complete
-            await asyncio.gather(*tasks, return_exceptions=True)
-
-if __name__ == '__main__':
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass
+# Install selenium
+pip install selenium
