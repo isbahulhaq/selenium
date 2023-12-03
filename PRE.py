c593895a6d7802770e3655c80c7be070edff92c3
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-03 18:30:35 +0530

    Update PRE.py

--- PRE.py
+++ PRE.py
@@ -12,10 +12,10 @@ subprocess.run(['pip', 'install', '--upgrade', 'webdriver_manager'])
 # Install curl
 subprocess.run(['apt', 'install', 'curl'])
 
-# Download the Brave browser archive keyring
+# Download Brave browser archive keyring
 subprocess.run(['curl', '-fsSLo', '/usr/share/keyrings/brave-browser-archive-keyring.gpg', 'https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg'])
 
-# Add Brave browser repository to sources.list
+# Add Brave browser repository to sources.list.d
 subprocess.run(['echo', 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main', '|', 'sudo', 'tee', '/etc/apt/sources.list.d/brave-browser-release.list'])
 
 # Update apt
